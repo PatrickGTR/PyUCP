41abe1d2188d18f7c06195ad32a8837a805da12f
PatrickGTR <patricksubang@live.com>
2019-05-16 17:34:31 +0100

    Better naming scheme, consistent formatting. - Using MySQL instead of mysql, using result_skill instead of just result - Better code format, use (double quote)  instead of (single quote) wrap if-statement, imports in paranthesis, etc... - Added dependency for functions (moved to functions.py) - Fixed an issue where the footer is different colour than the rest. - Added items and skills data

---
+++ modules/functions.py
@@ -0,0 +1,71 @@
+
+from modules import (
+    app, 
+    connect_sql
+)
+
+import pymysql
+
+account_data = {
+    "jobs": {
+        0: "Drug Dealer",
+        1: "Weapon Dealer",
+        2: "Hitman",
+        3: "Terrorist", 
+        4: "Rapist",
+        5: "Mechanic"
+    },
+    "skills":{
+        0: "Players Raped",
+        1: "Been Raped",
+        2: "Players Robbed",
+        3: "Been Robbed",
+        4: "Stores Robbed",
+        5: "Times Escaped Cuffs",
+        6: "Players Arrested",
+        7: "Times Been Arrested"
+    },
+    "items": {
+        0: "Weed",
+        1: "Crack",
+        2: "Rope",
+        3: "Lockpick",
+        4: "Scissors",
+        5: "Condoms",
+        6: "Wallet",
+        7: "C-4"
+    }
+}
+
+## app.template_global allows 
+## the functions below to be used in our .html files.
+
+@app.template_global()
+def retrieveNameFromID(accountid):
+    with connect_sql.MySQL() as c: 
+        c.execute(f"SELECT username FROM accounts WHERE accountid={accountid}")
+        result = c.fetchone()
+    return result["username"]
+
+@app.template_global()
+def getJobName(jobid):
+    jobName = account_data.get("jobs").get(jobid)
+
+    return jobName
+@app.template_global()
+def getSkillName(skillid):
+    skillName = account_data.get("skills").get(skillid)
+    return skillName
+
+@app.template_global()
+def getItemName(itemid):
+    itemName = account_data.get("items").get(itemid)
+    return itemName
+
+@app.template_global()
+def retrieveAdmins():
+    with connect_sql.MySQL() as c:
+        c.execute("SELECT userID, adminLevel FROM admins")
+        results = c.fetchall()
+    return results
+
--- modules/routes.py
+++ modules/routes.py
@@ -1,92 +1,114 @@
-from flask import Flask,render_template,request,session,url_for,redirect,flash
+from flask import (
+    Flask,
+    render_template,
+    request,session,
+    url_for,
+    redirect,
+    flash
+)
+
+from modules import (
+    app, 
+    connect_sql,
+    functions
+)
+
 import pymysql
 import hashlib
-
-from modules import app, connect_sql
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     
-    if session.get('remember_me'):
-        session['logged_in'] = True
-        username = session['username']
-        return render_template("index.html", admins=retrieveAdmins())
+    if(session.get("remember_me")):
+        session["logged_in"] = True
+        username = session["username"]
+        return render_template("index.html", 
+            admins=functions.retrieveAdmins()
+        )
 
-    if request.method != "POST":
-        return render_template("index.html", admins=retrieveAdmins())
+    if(request.method != "POST"):
+        return render_template("index.html", 
+            admins=functions.retrieveAdmins()
+        )
 
-    username = request.form.get('username')
-    password = request.form.get('password')
+    username = request.form.get("username")
+    password = request.form.get("password")
 
     with connect_sql.MySQL() as c:
         c.execute("SELECT accountID, password, hash FROM accounts WHERE username=%s", username)
         results = c.fetchone()
 
-
     if not results:
         flash("Invalid password or username, please try again.", "danger")
-        return render_template("index.html", admins=retrieveAdmins())
+        return render_template("index.html", 
+            admins=functions.retrieveAdmins()
+        )
 
-    retPassword, retSalt = results['password'], results['hash']
+    retPassword, retSalt = results["password"], results["hash"]
     password = hashlib.sha256(password.encode() + retSalt.encode()).hexdigest()
 
     if password.upper() != retPassword:
         flash("Wrong password, please try again.", "danger")
-        return render_template("index.html", admins=retrieveAdmins())
+        return render_template("index.html", 
+            admins=functions.retrieveAdmins()
+        )
 
     if request.form.get("checkbox"):
         session["remember_me"] = True
     
-    session['logged_in'] = True
-    session['username'] = results['accountID']
+    session["logged_in"] = True
+    session["username"] = results["accountID"]
     flash("Successfully logged in", "success")
     
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/dashboard/<int:accountid>', methods=["GET", "POST"])
+@app.route("/dashboard/<int:accountid>", methods=["GET", "POST"])
 def dashboard(accountid):
     with connect_sql.MySQL() as c:
-        c.execute(f"SELECT accountID, username, money, kills, deaths, jobID, score, experience, wantedlevel,  \
+        c.execute(f"SELECT *,  \
             DATE_FORMAT(registerdate, '%d %M %Y') as reg_date, \
             DATE_FORMAT(lastlogin, '%d, %M, %Y at %r') as last_log \
         FROM \
             accounts \
         WHERE \
             accountID={accountid}")
-        results = c.fetchone()
-
-
-    jobID = results['jobID']
-
-    jobs = {
-        0: "Drug Dealer",
-        1: "Weapon Dealer",
-        2: "Hitman",
-        3: "Terrorist",
-        4: "Rapist",
-        5: "Mechanic"
-    }
-    jobName = jobs.get(jobID)
+        result_account = c.fetchone()
     
-    return render_template("dashboard.html", results=results, jobName=jobName, admins=retrieveAdmins())
+    with connect_sql.MySQL() as c:
+        c.execute(f"\
+            SELECT \
+                type.skill_id, type.skill_name, skill.value \
+            FROM \
+                skills_type AS type \
+            LEFT JOIN \
+                skills_player AS skill \
+            ON \
+                skill.fk_skill_id = type.skill_id AND \
+                skill.fk_user_id = {accountid}")
+        result_skill = c.fetchall()
+    
+    with connect_sql.MySQL() as c:
+        c.execute(f"\
+            SELECT \
+                type.item_id, type.item_name, item.value \
+            FROM \
+                item_type AS type \
+            LEFT JOIN \
+                item_players AS item \
+            ON \
+                item.fk_item_id = type.item_id AND \
+                item.fk_user_id = {accountid}")
+        result_item = c.fetchall()
 
-@app.route('/logout')
+    return render_template("dashboard.html", 
+        item=result_item, 
+        skill=result_skill,
+        account=result_account, 
+        admins=functions.retrieveAdmins()
+    )
+
+@app.route("/logout")
 def logout():
     session.clear()
     flash("You have successfully logged out", "success")
-    return redirect(url_for('index'))
-
-def retrieveNameFromID(accountid):
-    with connect_sql.MySQL() as c: 
-        c.execute(f"SELECT username FROM accounts WHERE accountid={accountid}")
-        result = c.fetchone()
-    return result['username']
-
-def retrieveAdmins():
-    with connect_sql.MySQL() as c:
-        c.execute("SELECT userID, adminLevel FROM admins")
-        results = c.fetchall()
-        print(results)
-    return results
-
-app.jinja_env.globals.update(retrieveNameFromID=retrieveNameFromID)
+    return redirect(url_for("index"))
